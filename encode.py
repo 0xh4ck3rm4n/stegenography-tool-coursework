9926c366b41d22d007d6259ccaf786480cf3056e
Gaurav <gaurav.poudel2061@gmail.com>
2026-01-08 10:55:19 +0545

    added logic for px modification and capacity calc

--- encode.py
+++ encode.py
@@ -9,12 +9,22 @@ def text_to_binary(txt):
 
 # function to modify LSB
 def pixel_modify(pixel, bits):
-    pass
+    if bits == 1:
+        # Setting LSB to 1
+        return pixel | 1
+    else:
+        # Setting LSB to 0
+        return pixel & ~1
 
 # function to encode the message into pixel
-def encode_msg(pixel, message):
+def encode_msg(pixels, message):
     pass
 
 # function to calculate the maximum capacity of text to be merged in an image
 def calculate_capacity(img_size):
-    pass
+    # img_size : takes width and height as an argument
+    width, height = img_size
+    total_px = width * height
+    # Pixel is divided into 3 segments, and each char has 8 bits
+    maximum_characters = (total_px * 3) // 8 - 10
+    return maximum_characters

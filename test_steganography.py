1a7f34ba734ba226fa9c952fc0328b122d2bb0cf
Gaurav <gaurav.poudel2061@gmail.com>
2026-02-03 15:09:56 +0545

    imported required modules for test script

--- test_steganography.py
+++ test_steganography.py
@@ -1,3 +1,20 @@
 # Testing module for the tool
+import unittest
+from PIL import Image
+import img_operation as img_ops
+import encode
+import decode
 
 # TODO: Test function
+class TestImageOperations(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_validate_img_valid():
+        pass
+
+    def test_validate_img_none():
+        pass
+
+    def test_validate_img_small():
+        pass

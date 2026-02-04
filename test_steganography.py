e36be6b9122a475f33c5dd7c51b84a96e3e4a047
Gaurav <gaurav.poudel2061@gmail.com>
2026-02-04 12:27:25 +0545

    functionality logic added for encode unit testing

--- test_steganography.py
+++ test_steganography.py
@@ -45,23 +45,42 @@ class TestSteganographyEncode(unittest.TestCase):
 
     # This function test if it converts text to binary
     def test_text_to_binary(self):
-        pass
+        result = encode.text_to_binary("A")
+        self.assertEqual(result, "01000001")
+
+        result = encode.text_to_binary("AB")
+        self.assertEqual(len(result), 16)
 
     # This function test to set the LSB to 1
     def test_modify_pixel_to_one(self):
-        pass
+        result = encode.pixel_modify(10, '1')
+        self.assertEqual(result & 1, 1)
 
     # This function test if the pixel can be modified and set to zero
     def test_modify_pixel_set_to_zero(self):
-        pass
+        result = encode.pixel_modify(11, '0')
+        self.assertEqual(result & 1, 0)
 
     # This function test if the encode message has been successfully embedded
     def test_encode_message_success(self):
-        pass
+        pixels = [(100, 100, 100)] * 1000
+        message = "Hello"
+        result = encode.encode_msg(pixels, message)
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 1000)
     
     # This function test if the encode message is too large to fit in an image
     def test_encode_message_too_large(self):
-        pass
+        pixels = [(100, 100, 100)] * 1000
+        message = "This is a long long message" * 100
+        result = encode.encode_msg(pixels, message)
+        self.assertIsNone(result)
+    
+    # This function is to test for calculating capacity
+    def test_calculate_capacity(self):
+        capacity = encode.calculate_capacity((100, 100))
+        self.assertGreater(capacity, 0)
+        self.assertIsInstance(capacity, int)
 
 
 # Test case for Decoding Image

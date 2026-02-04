2f4e857fc7c07535ac93dd9cb38b828a218960e6
Gaurav <gaurav.poudel2061@gmail.com>
2026-02-04 11:56:04 +0545

    added two classes to test different cases for encode and decode

--- test_steganography.py
+++ test_steganography.py
@@ -5,7 +5,7 @@ import img_operation as img_ops
 import encode
 import decode
 
-# TODO: Test function
+# Test case for Image operation
 class TestImageOperations(unittest.TestCase):
     # This function is for setting up the test
     def setUp(self):
@@ -24,19 +24,34 @@ class TestImageOperations(unittest.TestCase):
     # This function is to check if it fails for the small image
     def test_validate_img_small(self):
         small_img = Image.new('RGB', (5, 5))
-        result = img_ops.img_valid(small_img)
+        result = img_ops.img_validation(small_img)
         self.assertFalse(result)
 
     # This function is to check if successfully gets the data from the Image
     def test_get_pixel_data(self):
-        pixel = img_ops.get_px_data(self.test_image)
+        pixel = img_ops.get_px_data(self.test_img)
         self.assertEqual(len(pixel), 10000)
         self.assertEqual(pixel[0], (255, 0, 0))
 
     # This function is to check if it successfully create image from the pixel data
     def test_create_image_from_pixel(self):
         pixels = [(255, 0, 0)] * 100
-        img = img_ops.img_create_from_px(pixels)
+        img = img_ops.img_create_from_px(pixels, (10, 10))
         self.assertEqual(img.size, (10, 10))
         self. assertEqual(img.mode, 'RGB')
+
+# Test case for Encoding Image
+class TestSteganographyEncode(unittest.TestCase):
+    pass
+
+# Test case for Decoding Image
+class TestSteganographyDecode(unittest.TestCase):
+    pass
+
+def run_tests():
+    unittest.main()
+
+if __name__ == '__main__':
+    run_tests()
+
         
